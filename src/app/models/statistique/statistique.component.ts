117b868818e7c39e993a49ac7ff98a78eabd4764
inesnbd <ines.nebbad@hotmail.fr>
2021-06-29 11:07:54 +0200

    exercice 2

---
+++ src/app/models/statistique/statistique.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistique',
+  templateUrl: './statistique.component.html',
+  styleUrls: ['./statistique.component.css']
+})
+export class StatistiqueComponent implements OnInit {
+  title = '2005';
+  title1 = '2021';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
